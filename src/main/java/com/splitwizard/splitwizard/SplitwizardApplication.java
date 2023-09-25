ae74f4f14d424bc28a51bba176fad2c1e496a9f4
PH010546 <philipinru@gmail.com>
2023-09-25 18:04:55 +0800

    init

---
+++ src/main/java/com/splitwizard/splitwizard/SplitwizardApplication.java
@@ -0,0 +1,13 @@
+package com.splitwizard.splitwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitwizard/splitwizard/SplitwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwizard.splitwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
