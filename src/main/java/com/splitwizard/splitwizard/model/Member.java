e768b8f2aff27cb8905e5f2e7bbff4e52c63e12e
PH010546 <philipinru@gmail.com>
2023-09-26 18:39:21 +0800

    modify code in model & service

--- src/main/java/com/splitwizard/splitwizard/model/Member.java
+++ src/main/java/com/splitwizard/splitwizard/model/Member.java
@@ -2,68 +2,23 @@ package com.splitwizard.splitwizard.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Getter @Setter
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String account;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Timestamp getCreated_time() {
-        return created_time;
-    }
-
-    public void setCreated_time(Timestamp created_time) {
-        this.created_time = created_time;
-    }
-
-    public Timestamp getUpdate_time() {
-        return update_time;
-    }
-
-    public void setUpdate_time(Timestamp update_time) {
-        this.update_time = update_time;
-    }
-
     private String name;
     private String password;
     private Timestamp created_time;
--- src/main/java/com/splitwizard/splitwizard/service/MemberService.java
+++ src/main/java/com/splitwizard/splitwizard/service/MemberService.java
@@ -5,8 +5,6 @@ import com.splitwizard.splitwizard.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.sound.midi.MetaMessage;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,32 +31,15 @@ public class MemberService {
 
     public String login(String account, String password){
 
-        // 把所有帳號密碼拉出來
-        List<Member> allMember = dao.findAll();
-        List<String> allAccount = new ArrayList<>();
-        List<String> allPassword = new ArrayList<>();
+        // 檢查帳號是否存在
+        if (dao.findByAccount(account) == null) return "帳號或密碼錯誤";
 
-
-        for (Member m : allMember){
-
-            allAccount.add(m.getAccount());
-            allPassword.add(m.getPassword());
-
-        }
-
-        // 檢查帳號
-        if (!allAccount.contains(account)) return "帳號或密碼錯誤";
-
-        // 檢查密碼
-        if (!allPassword.contains(password)) return "帳號或密碼錯誤";
 
         // 檢查帳號密碼是否符合
         Member member = dao.findByAccount(account);
         if (!member.getPassword().equals(password)) return "帳號或密碼錯誤";
 
         return "登入成功";
-
-
     }
 
 }
