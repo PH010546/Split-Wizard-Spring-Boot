5c38fb832fa320b2aa47ba9f2b8f4a5b822ef279
PH010546 <philipinru@gmail.com>
2023-09-28 00:17:10 +0800

    modify CORS configuration

--- src/main/java/com/splitwizard/splitwizard/controller/MemberController.java
+++ src/main/java/com/splitwizard/splitwizard/controller/MemberController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class MemberController {
 
