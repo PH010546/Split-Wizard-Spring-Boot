5ed4b39e1a694166a21ba066d85a30d728cf12d1
PH010546 <philipinru@gmail.com>
2023-09-27 18:05:52 +0800

    add Result.java

---
+++ src/main/java/com/splitwizard/splitwizard/Util/Result.java
@@ -0,0 +1,31 @@
+package com.splitwizard.splitwizard.Util;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter @Setter
+public class Result {
+
+    private Integer code;
+    private String message;
+    private Object result;
+
+    private Result setResult(int code, String message, Object data){
+
+        this.code = code;
+        this.message = message;
+        this.result = data;
+
+        return this;
+    }
+
+    public Result success(Object data){
+        return setResult(200, "Success", data);
+    }
+
+    public Result fail(String message){
+        return setResult(400, message, null);
+    }
+
+}
