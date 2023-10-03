3420f76411ebda99784fe7b590ec72069daaa8f1
PH010546 <philipinru@gmail.com>
2023-10-04 00:29:40 +0800

    modify addMemberToGroup

--- src/main/java/com/splitwizard/splitwizard/controller/MemberGroupConnController.java
+++ src/main/java/com/splitwizard/splitwizard/controller/MemberGroupConnController.java
@@ -3,6 +3,7 @@ package com.splitwizard.splitwizard.controller;
 import com.splitwizard.splitwizard.Util.Result;
 import com.splitwizard.splitwizard.model.MemberGroupConn;
 import com.splitwizard.splitwizard.service.MemberGroupConnServiceImpl;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberGroupConnController {
 
     private final MemberGroupConnServiceImpl service;
+    private final HttpSession session;
     @Autowired
-    public MemberGroupConnController(MemberGroupConnServiceImpl service){
+    public MemberGroupConnController(MemberGroupConnServiceImpl service, HttpSession session){
         this.service = service;
+        this.session = session;
     }
 
     @PostMapping(value = "/addMemberToGroup")
     public Result addMemberToGroup(@RequestBody MemberGroupConn mgc){
-        return service.addMemberToGroup(mgc.getMemberId(), mgc.getGroupId());
+        return service.addMemberToGroup((Integer) session.getAttribute("currentUser"), mgc.getGroupId());
     }
 
 }
