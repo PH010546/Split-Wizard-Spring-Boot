cc45ab2c4f3ac028cab406c05c5cc665aa852ad1
PH010546 <philipinru@gmail.com>
2023-10-05 17:46:39 +0800

    modify code

--- src/main/java/com/splitwizard/splitwizard/socket/SocketService.java
+++ src/main/java/com/splitwizard/splitwizard/socket/SocketService.java
@@ -1,22 +1,22 @@
-package com.splitwizard.splitwizard.socket;
-
-
-import com.corundumstudio.socketio.SocketIOClient;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-@Service
-@Slf4j
-public class SocketService {
-
-    public void sendMessage(String eventName, SocketIOClient senderClient, String message, Object data) {
-        for (
-                SocketIOClient client : senderClient.getNamespace().getAllClients()) {
-            if (!client.getSessionId().equals(senderClient.getSessionId())) {
-                client.sendEvent(eventName,
-                        new Message(MessageType.SERVER, message, data));
-            }
-        }
-    }
-
-}
+//package com.splitwizard.splitwizard.socket;
+//
+//
+//import com.corundumstudio.socketio.SocketIOClient;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@Slf4j
+//public class SocketService {
+//
+//    public void sendMessage(String eventName, SocketIOClient senderClient, String message, Object data) {
+//        for (
+//                SocketIOClient client : senderClient.getNamespace().getAllClients()) {
+//            if (!client.getSessionId().equals(senderClient.getSessionId())) {
+//                client.sendEvent(eventName,
+//                        new Message(MessageType.SERVER, message, data));
+//            }
+//        }
+//    }
+//
+//}
