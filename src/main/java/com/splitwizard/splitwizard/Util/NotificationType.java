6d297c1db3581ba1c65dc019aebc008e1f702857
PH010546 <philipinru@gmail.com>
2023-10-12 17:39:52 +0800

    modify NotificationType

--- src/main/java/com/splitwizard/splitwizard/Util/NotificationType.java
+++ src/main/java/com/splitwizard/splitwizard/Util/NotificationType.java
@@ -9,9 +9,13 @@ import java.util.Map;
 public enum NotificationType {
     SYSTEM(101, "System", "更新囉寶貝"),
     INVITATION(201, "Invitation", " 已邀請您至 "),
+
     ITEM_ADD(301, "Item", " 中有新增紀錄"),
     ITEM_UPDATE(302, "Item", " 中有更新紀錄"),
-    ITEM_DELETE(303, "Item", " 中有紀錄被刪除");
+    ITEM_DELETE(303, "Item", " 中有紀錄被刪除"),
+
+    RESULT_PAY(401, "Result", "支付帳款"),
+    RESULT_UNPAY(402, "Result", "取消支付");
 
     private final Integer code;
     private final String type;
