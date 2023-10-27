c433414bfda5beb340ecfecf329d806225bb5dde
PH010546 <philipinru@gmail.com>
2023-10-27 16:13:51 +0800

    clean the code in WebSecurityConfig

--- src/main/java/com/splitwizard/splitwizard/Util/WebSecurityConfig.java
+++ src/main/java/com/splitwizard/splitwizard/Util/WebSecurityConfig.java
@@ -28,32 +28,10 @@ public class WebSecurityConfig{
         this.jwtAuthFilter = jwtAuthFilter;
     }
 
-
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
         http.
-//                csrf(AbstractHttpConfigurer::disable)
-//                .userDetailsService(userDetailsService)
-//                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/login", "/register", "/auth", "/auth/parse").permitAll()
-//                        .requestMatchers("/auth").permitAll()
-//                        .requestMatchers("/auth/parse").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin(form -> form
-//                        .loginPage("http://localhost:3000/splitWizard/login")
-//                        .permitAll()
-//                )
-//                .logout(LogoutConfigurer::permitAll)
-                // make single user can only use one session at a time, and will prevent the second login
-//                .sessionManagement(session -> session
-//                        .maximumSessions(1)
-//                        .maxSessionsPreventsLogin(true))
-//                .logout((logout) -> logout.logoutUrl("/logout").logoutSuccessUrl("/groups"));
-
                 // 因為這裡設定 sessionCreationPolicy(SessionCreationPolicy.STATELESS)，所以
                 // 不用 http session 記錄資料，因此 SecurityContextHolder 不會將登入認証成功後的 Authentication 記到
                 // http session。也就是說登入認証成功後的 Authentication 不會被記錄到系統，每次的 http request 都必需重新
@@ -68,15 +46,9 @@ public class WebSecurityConfig{
                         .requestMatchers("/login", "/register").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//                .logout((logout) -> logout.logoutUrl("/logout"));
         return http.build();
 
     }
-
-//    @Bean
-//    public HttpSessionEventPublisher httpSessionEventPublisher() {
-//        return new HttpSessionEventPublisher();
-//    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
