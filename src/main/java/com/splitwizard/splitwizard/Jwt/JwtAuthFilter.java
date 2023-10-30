e109f9f46fce8e2c0e183ea5f9762a26716697a5
PH010546 <philipinru@gmail.com>
2023-10-30 18:44:38 +0800

    modify annotations before parameter to avoid exception

--- src/main/java/com/splitwizard/splitwizard/Jwt/JwtAuthFilter.java
+++ src/main/java/com/splitwizard/splitwizard/Jwt/JwtAuthFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,9 +40,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(@NotNull HttpServletRequest request,
-                                    @NotNull HttpServletResponse response,
-                                    @NotNull FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) // 這裡不使用@NotNull是因為會造成錯誤
             throws ServletException, IOException {
         try {
             String token = retriveJwt(request);
